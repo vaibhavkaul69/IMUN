2e310c4bb3122f3903df1f26552cd68f0832e08d
unknown <vaibhav.kaul2@gnmail.com>
2019-12-27 16:36:29 +0530

    Responsive

---
+++ js/reviews.js
@@ -0,0 +1,13 @@
+ var swiper = new Swiper('.swiper-reviews', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
